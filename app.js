bb03531c895fdbd2f15f9d157291f8ccced7af92
chetan <chetanchoudhary435@gmail.com>
2025-09-05 12:58:48 +0530

    css added

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
-function playAudio() {
-      let audio = document.getElementById("myAudio");
 
-      if (!audio.src || audio.src.trim() === "") {
-        alert("No audio available for this word");
-        return;
-    }
-    
-     audio.play()
-        .then(() => {
-            console.log("Audio playing...");
-        })
-        .catch(() => {
-            alert("Unable to play audio");
-        });
 
-      
-    }
+async function playAudio(url) {
+  try {
+    let audio =document.querySelector("audio");
+    audio.src=url;
+    await audio.play();
+    console.log("Playing:", url);
+  } catch (err) {
+    alert("Unable to play:", err);
+  }
+}
+
+
 
 // let btn=document.querySelector("button");
 
@@ -26,14 +22,37 @@ function playAudio() {
 
 let url="https://api.dictionaryapi.dev/api/v2/entries/en/";
 let inp=document.querySelector("input");
+let btn=document.querySelector("button");
+
+inp.addEventListener("input",()=>{
+    if (inp.value.trim() !== "") {
+      btn.style.opacity = "1";   // Visible
+      let p=document.querySelector("p");
+      p.innerText="";
+
+      let playbutton=document.querySelector(".play");
+      playbutton.style.display="none";
+    } else {
+      btn.style.opacity = "0.5"; // Faded again if empty
+    }
+})
+
+
 
 async function getmeaning(finalurl){
     try{
         let res= await axios.get(finalurl);
-        return res.data[0].meanings[0].definitions[0].definition;
+        let meaning= res.data[0].meanings[0].definitions[0].definition;
+        let p=document.querySelector("p");
+        p.innerText="";
+        p.innerText=meaning;
+        p.style.color="black";
     }
     catch(e){
-        return e;
+        let p=document.querySelector("p");
+        p.innerText="";
+        p.innerText=e;
+        p.style.color="red";
     }
 }
 
@@ -41,6 +60,8 @@ async function getaudio(finalurl){
     try{
         let res= await axios.get(finalurl);
         return res.data[0].phonetics[0].audio;
+        
+        
     }
     catch(e){
         alert("error",e)
@@ -48,27 +69,39 @@ async function getaudio(finalurl){
     }
 }
 
-let btn=document.querySelector("button");
+
 btn.addEventListener("click", async()=>{
+
+
+    if(inp.value.trim() === ""){
+        let p=document.querySelector("p");
+        p.innerText="";
+        p.innerText="enter a word first!!";
+        p.style.color="red";
+    }
+    else{
+    
     let finalurl=url+inp.value;
-    let meaning= await getmeaning(finalurl);
-    // console.log(meaning);
-    let p=document.querySelector("p");
-    p.innerText="";
-    p.innerText=meaning;
+    await getmeaning(finalurl);
+    
+    
 
     let btn1=document.createElement("button");
-    btn1.innerText="play";
-    p.appendChild(btn1);
+    btn1.innerHTML='<i class="fa-solid fa-volume-high"></i>';
+    btn1.classList.add("play");
+    let mainbox=document.querySelector(".mainbox");
+    mainbox.lastElementChild.remove();
+    mainbox.appendChild(btn1);
 
     
 
     btn1.addEventListener("click",async ()=>{
-        let audiourl=await getaudio(finalurl);
-        let audio = document.getElementById("myAudio");
-        audio.src=audiourl;
-        playAudio();
+        let finalurl=url+inp.value;
+        let audiourl = await getaudio(finalurl);
+        await playAudio(audiourl);
+        
     })
+}
    
     
  })
